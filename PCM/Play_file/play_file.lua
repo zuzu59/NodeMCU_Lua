e09770c2407a736fbd31c85b00cb793735cd2e3c
Christian Zufferey <christian@zufferey.com>
2019-01-31 00:25:43 +0100

    Ecrit un tout mini player MP3 ;-)

---
+++ PCM/Play_file/play_file.lua
@@ -0,0 +1,47 @@
+-- Scripts pour jouer un fichier son sur un HP
+
+print("\n play_file.lua zf190130.2335 \n")
+
+
+-- ****************************************************************************
+-- Play file with pcm module.
+--
+-- Upload jump_8k.u8 to spiffs before running this script.
+--
+-- ****************************************************************************
+
+
+function cb_drained(d)
+  print("drained "..node.heap())
+
+  file.seek("set", 0)
+  -- uncomment the following line for continuous playback
+  --d:play(pcm.RATE_8K)
+end
+
+function cb_stopped(d)
+  print("playback stopped")
+  file.seek("set", 0)
+end
+
+function cb_paused(d)
+  print("playback paused")
+end
+
+file.open("toto.wav", "r")
+
+drv = pcm.new(pcm.SD, 5)
+
+-- fetch data in chunks of FILE_READ_CHUNK (1024) from file
+drv:on("data", function(drv) return file.read() end)
+
+-- get called back when all samples were read from the file
+drv:on("drained", cb_drained)
+
+drv:on("stopped", cb_stopped)
+drv:on("paused", cb_paused)
+
+-- start playback
+drv:play(pcm.RATE_16K)
+
+
