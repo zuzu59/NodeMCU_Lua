70080531f242439ff7cffebd231f6b18948bf88e
Christian Zufferey <christian@zufferey.com>
2019-08-08 11:34:54 +0200

    Corrigé le problème du nouveau timer

--- Workshop/181015/hello_world/blink_led1.lua
+++ Workshop/181015/hello_world/blink_led1.lua
@@ -1,6 +1,6 @@
 -- programme pour faire clignoter une LED avec un rapport on/off
 
-print("\n blink_led1.lua zf181116.0014 \n")
+print("\n blink_led1.lua zf190808.1134 \n")
 
 zLED=0
 zTm_On_LED = 50    --> en ms
@@ -10,10 +10,10 @@ zFlag_LED = 0
 function blink_LED ()
     if zFlag_LED==gpio.LOW then 
         zFlag_LED=gpio.HIGH
-        tmr.alarm(ztmr_LED, zTm_Off_LED, tmr.ALARM_SINGLE, blink_LED)
+        ztmr_LED:alarm(zTm_Off_LED, tmr.ALARM_SINGLE, blink_LED)
     else 
         zFlag_LED=gpio.LOW
-        tmr.alarm(ztmr_LED, zTm_On_LED, tmr.ALARM_SINGLE, blink_LED)
+        ztmr_LED:alarm(zTm_On_LED, tmr.ALARM_SINGLE, blink_LED)
     end
     gpio.write(zLED, zFlag_LED)
 end
