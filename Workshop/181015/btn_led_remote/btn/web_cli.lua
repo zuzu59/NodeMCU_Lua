b90e83ff17256d24c52bfbba68581e8bc37b9241
Christian Zufferey <christian@zufferey.com>
2019-08-08 16:05:35 +0200

    wip

--- Workshop/181015/btn_led_remote/btn/web_cli.lua
+++ Workshop/181015/btn_led_remote/btn/web_cli.lua
@@ -1,10 +1,10 @@
 -- Petit script pour envoyer quelque chose sur un serveur WEB
-print("\n web_cli.lua   zf181015.1625   \n")
+print("\n web_cli.lua   zf190808.1605   \n")
 
 function disp_send()
 --    http.get("http://192.168.4.1/?line1="..zlength.."m", nil, function(code, data)
     print("web_cli: "..zled_state)
-    http.get("http://192.168.4.1/?pin="..zled_state, nil, function(code, data)
+    http.get("http://192.168.43.158/?pin="..zled_state, nil, function(code, data)
         if (code < 0) then
           print("HTTP request failed")
         else
