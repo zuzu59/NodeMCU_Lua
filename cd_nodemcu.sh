d69ae11a6e5bcb4635f2bc5a622ae1c2ef9b2a6e
Christian Zufferey <christian@zufferey.com>
2019-09-16 14:35:17 +0200

    Ajouté deux petits scripts bash pour me simplifier la vie ;-)

---
+++ cd_nodemcu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Petit script pour aller facilement dans le dossier NodeMCU
+#zf190916.1433
+
+cd /Users/zuzu/Desktop/NodeMCU/NodeMCU_Lua
+
+
---
+++ start_nodemcu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Petit script pour démarrer facilement l'IDE pour le NodeMCU
+#zf190709.2123
+
+cd /Users/zuzu/Desktop/NodeMCU/NodeMCU_Lua
+./start_ESPlorer.sh
+
+
