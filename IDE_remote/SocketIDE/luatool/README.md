0a161206d1d9ef39e7f42a4ba6f1c2a5c2c45aba
Christian Zufferey <christian@zufferey.com>
2019-10-20 18:38:16 +0200

    wip

--- IDE_remote/SocketIDE/luatool/README.md
+++ IDE_remote/SocketIDE/luatool/README.md
@@ -3,12 +3,15 @@
 ## Sources
 https://github.com/4refr0nt/luatool
 
+
 ## Description
 Le petit script python *luatool.py* permet d'automatiser le téléchargement de
 scripts *.lua* sur un NodeMCU branché sur le port USB.
 
+
 ## Problématiques
-La version actuelle datant de 2017 ne fonctionne plus avec les nouveaux firmwares de NodeMCU, il a y un problème de délai au moment de l'initialisation
+La version actuelle datant de 2017 ne fonctionne plus avec les nouveaux firmwares de NodeMCU, il a y un problème de délai au moment de l'initialisation.
+
 
 ## Corrections
 * J'ai donc repris le code python et mis un petit délai de 0.5 secondes juste après l'initialisation du port série.
@@ -18,5 +21,15 @@ La version actuelle datant de 2017 ne fonctionne plus avec les nouveaux firmware
 * J'ai aussi mis un nouveau telnet serveur qui tient compte des tailles maximales des paquets TCP
 
 
+## Utilisation
+Maintenant c'est très facile de télécharger tout un projet dans un NodeMCU. Il suffit simplement de modifier le petit script bash upload.sh, puis de faire:
 
-zf191020.1819
+```
+./upload.sh
+```
+S'il y a des erreurs lors d'un téléchargement, il faut simplement augmenter un peu le délai !
+
+
+
+
+zf191020.1836
--- IDE_remote/SocketIDE/luatool/upload.sh
+++ IDE_remote/SocketIDE/luatool/upload.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # Petit script pour télécharger facilement tout le binz
-#zf191020.1745
+#zf191020.1837
+
+# S'il y a des erreurs lors d'un téléchargement, il faut simplement augmenter un peu le délai !
 
 chmod +x luatool.py
 ./luatool.py --port /dev/cu.wchusbserial1410 -l
-./luatool.py --port /dev/cu.wchusbserial1410 -f websocket.lua
-./luatool.py --port /dev/cu.wchusbserial1410 -f main.lua
-./luatool.py --port /dev/cu.wchusbserial1410 -f init.lua
+./luatool.py --port /dev/cu.wchusbserial1410 -w
+./luatool.py --port /dev/cu.wchusbserial1410 -l
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.001 -f boot.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.001 -f boot2.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.03 -f flash_led_xfois.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.001 -f initz.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.001 -f secrets_energy.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.001 -f wifi_ap_stop.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.001 -f wifi_cli_conf.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.001 -f wifi_cli_start.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.001 -f wifi_info.lua
+./luatool.py --port /dev/cu.wchusbserial1410 --delay 0.04 -f telnet_srv2.lua
 ./luatool.py --port /dev/cu.wchusbserial1410 -l
