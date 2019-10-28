524254d3b6a868fa3e7ba221f07e530688955e3b
Christian Zufferey <christian@zufferey.com>
2019-10-28 15:37:08 +0100

    wip

--- WIFI_sniffer/msl_project/upload.sh
+++ WIFI_sniffer/msl_project/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Petit script pour télécharger facilement tout le binz
-#zf191028.1404
+#zf191028.1459
 
 # S'il y a des erreurs lors d'un téléchargement, il faut simplement augmenter un peu le délai !
 # Il est préférable de télécharger en premier les *gros* fichiers .lua !
@@ -20,13 +20,13 @@ chmod +x luatool.py
 read -p "Est-ce bien vide ?"
 
 ./luatool.py --port $luatool_tty --bar --delay 0.06 -f telnet_srv2.lua
+./luatool.py --port $luatool_tty --bar --delay 0.03 -f head.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.03 -f flash_led_xfois.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.001 -f wifi_info.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.001 -f wifi_cli_start.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.001 -f wifi_cli_conf.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.001 -f wifi_ap_stop.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.001 -f secrets_energy.lua
-./luatool.py --port $luatool_tty --bar --delay 0.001 -f head.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.001 -f dir.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.001 -f cat.lua
 ./luatool.py --port $luatool_tty --bar --delay 0.001 -f boot2.lua
