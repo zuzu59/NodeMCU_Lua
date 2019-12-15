09a41ab6dc8d9a0e0232de7c712eff9ad87b2b24
Christian Zufferey <christian@zufferey.com>
2019-12-15 13:56:45 +0100

    wip, cela ne marche vraiment pas du tout :-(

--- Mesures/humidity/bolo/0_get_data.lua
+++ Mesures/humidity/bolo/0_get_data.lua
@@ -1,5 +1,5 @@
 -- Lit le capteur I2C HTU21D de mesure d'humidité et de température
-print("\n 0_get_data.lua zf191214.1337 \n")
+print("\n 0_get_data.lua zf191215.1329 \n")
 
 -- https://cdn-shop.adafruit.com/datasheets/1899_HTU21D.pdf
 -- Comparaison DHT22, AM2302, AM2320, AM2321, SHT71, HTU21D, Si7021, BME280
@@ -10,19 +10,48 @@ print("\n 0_get_data.lua zf191214.1337 \n")
 -- source2: https://github.com/tebben/NodeMCU
 -- source3: https://github.com/codywon/NodeMCU-2
 
+scl = 1
+sda = 2
+id = 0
+addr = 0x40
+HUMIDITY = 0xE5
+TEMPERATURE = 0xE3
+SOFTRESET = 0xFE
+
+print(scl, sda, id, addr, HUMIDITY, TEMPERATURE, SOFTRESET)
+
+zspeed = i2c.setup(id, sda, scl, 500000)
+print(zspeed)
+
+i2c.start(id)
+zerr = i2c.address(id, addr, i2c.TRANSMITTER)
+i2c.stop(id)
+
+print("ack: ",zerr)
 
 
+--[[
 
-function read_mesure()
+i2c.write(id, SOFTRESET)
+i2c.stop(id)
 
-    local sda, scl = 6, 5
-    i2c.setup(0, sda, scl, i2c.SLOW) -- call i2c.setup() only once
-    si7021.setup()
-
-    fwrev = si7021.firmware()
-    print(string.format("FW: %X\r\n", fwrev))
-
-end
+tmr.delay(20*1000)
 
 
-read_mesure()
+i2c.start(id)
+i2c.address(id, addr, i2c.TRANSMITTER)
+i2c.write(id, HUMIDITY)
+i2c.stop(id)
+
+i2c.start(id)
+i2c.address(id, addr, i2c.RECEIVER)
+tmr.delay(50*1000)
+r = i2c.read(id,3)
+i2c.stop(id)
+
+
+print(string.byte(r))
+print(string.byte(r,2))
+print(string.byte(r,3))
+
+]]
