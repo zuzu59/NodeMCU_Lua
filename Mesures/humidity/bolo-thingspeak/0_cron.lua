48b41042bf5e9bbb478606ae2a25bd6ade3a5198
Christian Zufferey <christian@zufferey.com>
2020-01-19 09:54:38 +0100

    Mis en service le mode verbose afin de pas polluer la console telnet avec tous les *prints* utilisés pour les debugs

--- Mesures/humidity/bolo-thingspeak/0_cron.lua
+++ Mesures/humidity/bolo-thingspeak/0_cron.lua
@@ -1,16 +1,14 @@
--- Petit script pour faire office de crontab pour les mesures de température
-print("\n 0_cron.lua   zf200115.1704   \n")
+-- Petit script pour faire office de crontab pour les mesures
+print("\n 0_cron.lua   zf200119.0947   \n")
 
 cron1=tmr.create()
 cron1:alarm(20*1000,  tmr.ALARM_AUTO, function()
-    print("cron1........................")
-    ztemp1=readTemp()
-    zhum1=readHumi()
-    print("Temperature: "..ztemp1.." °C")
-    print("Humidity: "..zhum1.." %")
+    if verbose then print("cron1........................") end
+    if verbose then gpio.write(zLED, gpio.LOW) tmr.delay(10000) gpio.write(zLED, gpio.HIGH) end
+    ztemp1=readTemp()   zhum1=readHumi()
+    if verbose then print("Temperature: "..ztemp1.." °C") end
+    if verbose then print("Humidity: "..zhum1.." %") end
     zurl=thingspeak_url.."field1="..tostring(ztemp1).."&field2="..tostring(zhum1)
     send_temp()
+    if verbose then print(node.heap()) collectgarbage() print(node.heap()) end
 end)
-
-
-    
--- Mesures/humidity/bolo-thingspeak/0_send_data.lua
+++ Mesures/humidity/bolo-thingspeak/0_send_data.lua
@@ -1,25 +1,20 @@
 -- Petit script pour envoyer les valeurs de température sur un serveur WEB via un HTTP GET
-print("\n 0_send_data.lua   zf200115.1705   \n")
+print("\n 0_send_data.lua   zf200119.0932   \n")
 
 function send_temp()
-    print("send_web_temp: ")
-    print(zurl)
+    if verbose then print("send_web_temp: ") end
+    if verbose then print(zurl) end
 
     http.get(zurl, nil, function(code, data)
---            print("toto")
             if (code < 0) then
-                print("HTTP request failed")
-                print("zuzu", code, data)
+                if verbose then print("HTTP request failed") end
+                if verbose then print("zuzu", code, data) end
             else
-                print(code, data)
+                if verbose then print(code, data) end
             end
---            print("tutu")
         end)
---        print("titi")
 end
 
 --[[
-
 send_temp()
-
 ]]
