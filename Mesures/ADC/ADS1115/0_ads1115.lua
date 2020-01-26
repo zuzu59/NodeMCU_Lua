564d08bb0b38c4bfe68138bff0882129f1a104db
Christian Zufferey <christian@zufferey.com>
2020-01-26 15:28:29 +0100

    J'arrive à lire l'ADC0 et l'afficher, c'est déjà un bon début ;-)

---
+++ Mesures/ADC/ADS1115/0_ads1115.lua
@@ -0,0 +1,39 @@
+-- Lit le le petit convertisseur ADC ADS1115
+
+function readADS1115()
+    if verbose then print("\n 0_ads1115.lua   zf200126.1525 \n") end
+    
+    id = 0   sda = 5   scl = 6   addr = 0x48
+    
+    i2c.setup(id, sda, scl, i2c.SLOW)   sda = nil   scl = nil
+
+    function read_ADS1115(ADCnum)
+        ads1115.reset()
+        adc1 = ads1115.ads1115(id, addr)
+        
+        -- single shot
+        adc1:setting(ads1115.GAIN_6_144V, ads1115.DR_128SPS, ads1115.SINGLE_0, ads1115.SINGLE_SHOT)
+        -- start adc conversion and get result in callback after conversion is ready
+        adc1:startread(function(volt, volt_dec, adc, sign) print(volt, volt_dec, adc, sign) end)
+        
+    
+    end
+
+--[[
+if verbose then print("ADC0: "..read_ADS1115(0).." V") end
+
+    id=nil sda=nil scl=nil addr=nil HUMIDITY=nil TEMPERATURE=nil r=nil
+    read_HTU21D=nil readTemp=nil readHumi=nil readHTU21D=nil
+]]
+    if verbose then print(node.heap()) end
+    collectgarbage()
+    if verbose then print(node.heap()) end
+end
+
+readADS1115()
+read_ADS1115(0)
+
+--[[
+read_ADS1115(0)
+
+]]
