b1dfd3ccfc3d0c6810f01cbc7d468b757aa33636
Christian Zufferey <christian@zufferey.com>
2020-01-26 17:26:45 +0100

    wip

--- Mesures/ADC/ADS1115/0_ads1115.lua
+++ Mesures/ADC/ADS1115/0_ads1115.lua
@@ -1,7 +1,7 @@
 -- Lit le le petit convertisseur ADC ADS1115
 
 function readADS1115()
-    if verbose then print("\n 0_ads1115.lua   zf200126.1525 \n") end
+    if verbose then print("\n 0_ads1115.lua   zf200126.1709 \n") end
     
     id = 0   sda = 5   scl = 6   addr = 0x48
     
@@ -10,13 +10,26 @@ function readADS1115()
     function read_ADS1115(ADCnum)
         ads1115.reset()
         adc1 = ads1115.ads1115(id, addr)
-        
+
         -- single shot
-        adc1:setting(ads1115.GAIN_6_144V, ads1115.DR_128SPS, ads1115.SINGLE_0, ads1115.SINGLE_SHOT)
+--        adc1:setting(ads1115.GAIN_6_144V, ads1115.DR_128SPS, ads1115.SINGLE_0, ads1115.SINGLE_SHOT)
+        -- continuous mode
+        adc1:setting(ads1115.GAIN_6_144V, ads1115.DR_128SPS, ads1115.SINGLE_0, ads1115.CONTINUOUS)
+
         -- start adc conversion and get result in callback after conversion is ready
-        adc1:startread(function(volt, volt_dec, adc, sign) print(volt, volt_dec, adc, sign) end)
-        
-    
+--        adc1:startread(function(volt, volt_dec, adc, sign) 
+--            print("startread",volt, volt_dec, adc, sign) 
+--        end)
+
+        -- continuous mode
+--        adc1:setting(ads1115.GAIN_6_144V, ads1115.DR_128SPS, ads1115.SINGLE_0, ads1115.CONTINUOUS)
+        tmr.delay(18*1000)
+        t1=tmr.now()
+        volt, volt_dec, adc, sign = adc1:read()
+        t2=tmr.now()
+        print("read, t2-t1: ",(t2-t1)/1000)
+        print(volt, volt_dec, adc, sign) 
+   
     end
 
 --[[
