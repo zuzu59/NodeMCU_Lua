3342bc05acd085032118ced771dfa387c4a3a865
Christian Zufferey <christian@zufferey.com>
2020-02-19 14:13:34 +0100

    Cela ne marche plus :-((

--- Mesures/ADC/ADS1115/0_tst3_socat.lua
+++ Mesures/ADC/ADS1115/0_tst3_socat.lua
@@ -10,7 +10,7 @@ socat TCP-LISTEN:23047,reuseaddr,fork TCP-LISTEN:24047,reuseaddr,bind=127.0.0.1
 telnet -r localhost 24047
 ]]
 
-print("\n 0_tst3_socat.lua   zf200217.0822   \n")
+print("\n 0_tst3_socat.lua   zf200219.1412   \n")
 
 local node, table, tmr, wifi, uwrite,     tostring =
       node, table, tmr, wifi, uart.write, tostring
@@ -108,11 +108,12 @@ print("Telnet server running...\nUsage: telnet -rN ip\n")
 function rt_connect()
     srv_rt = net.createConnection(net.TCP, 0)
     srv_rt:on("connection", function(sck)
-        if verbose then print("connected on "..console_host..":"..console_port) end
+        if verbose then print("connected zzz on "..console_host..":"..console_port+yellow_tag) end
         telnet_listener(sck)
         print("Welcome to NodeMCU world.")
     end)
-    srv_rt:connect(console_port+yellow_tag,console_host)
+    --srv_rt:connect(console_port+yellow_tag,console_host)
+    srv_rt:connect(23047,console_host)
 end
 
 tmr_socat1=tmr.create()
