e1cd0eb2af888bfaae6a1b828e23fb92fa4a155a
Christian Zufferey <christian@zufferey.com>
2020-05-24 13:37:07 +0200

    Mon fifo http_post à l'air de fonctionner à la mano, me reste encore à le mettre en auto dans le crontab :-)

--- Mesures/humidity/solar-grafana/0_cron.lua
+++ Mesures/humidity/solar-grafana/0_cron.lua
@@ -1,5 +1,15 @@
 -- Petit script pour faire office de crontab pour les mesures
-print("\n 0_cron.lua   zf200524.1224   \n")
+print("\n 0_cron.lua   zf200524.1246   \n")
+
+
+function tprint(t)    
+   for key,value in pairs(t) do
+       print(key, value)
+   end
+end
+
+ztemp1=20  zhum1=40   ztemp2=20  zhum2=40
+
 
 cron1=tmr.create()
 cron1:alarm(5*1000,  tmr.ALARM_AUTO, function()
@@ -8,6 +18,22 @@ cron1:alarm(5*1000,  tmr.ALARM_AUTO, function()
 
     f = "0_1_htu21d.lua"  if file.exists(f) then dofile(f) end
     f = "0_2_htu21d.lua"  if file.exists(f) then dofile(f) end
+    
+
+
+    http_post(influxdb_url,"energy,capteur=th1 humidity="..ztemp1)
+    http_post(influxdb_url,"energy,capteur=th1 humidity="..zhum1)
+    http_post(influxdb_url,"energy,capteur=th1 humidity="..ztemp2)
+    http_post(influxdb_url,"energy,capteur=th1 humidity="..zhum2)
+    
+    
+    
+    
+
+    table.remove(t_zurl, 1)  table.remove(t_zarg, 1)
+    print("t_zurl:")  tprint(t_zurl)   print("t_zarg:")  tprint(t_zarg)
+
+    
     f = "0_send_data.lua" if file.exists(f) then dofile(f) end
     ztemp1=nil  zhum1=nil  ztemp2=nil  zhum2=nil
     f = "0_zdyndns.lua"   if file.exists(f) then dofile(f) end
--- Mesures/humidity/solar-grafana/boot.lua
+++ Mesures/humidity/solar-grafana/boot.lua
@@ -1,16 +1,18 @@
 -- Scripts à charger après le boot pour démarrer son projet
 
-print("\n boot.lua zf200523.1833 \n")
+print("\n boot.lua zf200524.1250 \n")
 
 function boot()
     verbose = true
     print("On lance le boot...")
     print(node.heap()) collectgarbage() print(node.heap())
+    
     --f="0_tst3_socat.lua"   if file.exists(f) then dofile(f) end
-    f="0_btn_flipflop.lua"   if file.exists(f) then dofile(f) end
-    f="0_cron.lua"   if file.exists(f) then dofile(f) end
+    --f="0_btn_flipflop.lua"   if file.exists(f) then dofile(f) end
+    --f="0_cron.lua"   if file.exists(f) then dofile(f) end
 
     --f = "web_ide2.lua"   if file.exists(f) then dofile(f) end
+    
     print("verbose:",verbose)
     print("boot lancé...")
     print(node.heap()) collectgarbage() print(node.heap())
