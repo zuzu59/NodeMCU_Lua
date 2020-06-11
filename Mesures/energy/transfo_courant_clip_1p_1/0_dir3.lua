05a879f1a8175cd8c3523949063afb8f23999c37
Christian Zufferey <christian@zufferey.com>
2020-06-11 16:51:43 +0200

    wip, cela commence enfin à marcher

--- Mesures/energy/transfo_courant_clip_1p_1/0_dir3.lua
+++ Mesures/energy/transfo_courant_clip_1p_1/0_dir3.lua
@@ -1,82 +1,61 @@
 -- fonction dir_vers() pour afficher toutes les versions de tous les fichiers *.lua sur le NodeMCU !
 -- fonction filec(fichier) pour afficher la version d'un seul fichiers sur le NodeMCU !
 
-print("\n 0_dir3.lua   zf200611.1519   \n")
-
--- function calc_chksum_file()
---     local name_file = list_files[zcmpt1]
---     print(name_file)
---     local size_file = 1   local chksum_file = 0
---     local f = file.open(name_file, "r")
---     while true do
---         local t = f:read(1)   if t == nil then break end
---         chksum_file = chksum_file + size_file * string.byte(t)
---         size_file = size_file + 1
---         if size_file%100 == 0 then uart.write(0,".") end
---     end
---     f:close()   print("")
---     zdir[#zdir+1]=name_file..string.rep(" ",24-string.len(name_file)).." : "..size_file..", "..chksum_file
---     zcmpt1 = zcmpt1 + 1
---     zrepeat()
--- end
+print("\n 0_dir3.lua   zf200611.1651   \n")
 
 function file_vers(name_file)
-    print(name_file)
-    zdir[#zdir+1]=name_file
-end
-     
-    
-    
-    
-    
---     print(name_file)
---     local f = file.open(name_file, "r")
---     while true do
---         local t = f:readline()   if t == nil then break end
---         print("/"..t.."/")
---         -- recherche de l'entête de version [print("\n ]
---         -- astuce, il faut échapper la ( avec un % et convertir le \ en char(92)
---         k='print%("'..string.char(92).."n "
---         i,j = string.find(t,k)
---         print(i,j)
--- 
---         if i ~= nil then
---             print(t)
---             break
---         end
---         uart.write(0,".")
---     end
---     f:close()   print("")
---     -- print(name_file..string.rep(" ",24-string.len(name_file)).." : "..size_file..", "..chksum_file)
--- end
+    local z=""
+    if string.find(name_file,"%.lua") then
+        z=name_file..":"
+        -- print("fichier: "..name_file)
+        local f,i1,i2,j1,j2,k,t = f,i1,i2,j1,j2,k,t
+        f = file.open(name_file, "r")
+        while true do
+            local t = f:readline()   if t == nil then break end
+    --        print("/"..t.."/")
+            -- recherche de l'entête de version [print("\n ]
+            -- astuce, il faut échapper la ( avec un % et convertir le \ en char(92)
+            k='print%("'..string.char(92).."n "
+            i1,j1 = string.find(t,k)
 
---[[
-file_vers("dir.lua")
-file_vers("dir3.lua")
-file_vers("cat.lua")
-]]
+    --        print(i,j)
 
-function zrepeat()
-    if zcmpt1 <= #list_files then
-        -- node.task.post(calc_chksum_file)
-        --node.task.post(file_vers(list_files[zcmpt1]))
-        file_vers(list_files[zcmpt1])
-        
-    else
-        table.sort(zdir)   for i=1, #zdir do   print(zdir[i])   end
-        zdir=nil   list_files=nil   zcmpt1=nil
+            if i1 ~= nil then
+                k=string.char(92)..'n"%)'
+                i2,j2 = string.find(t,k,j1)
+--                print(t)
+--                print(i1,j1,i2,j2)
+                z=name_file..": "..string.sub(t,j1+1,i2-2)
+--                print(z)
+                break
+            end
+--            uart.write(0,".")
+        end
+        f:close()
+        print(z)        
     end
+    return z
 end
 
 function dir_vers()
     zdir={}   list_files={}
-    local pfile = file.list()
+    local k,v = k,v   local pfile = file.list()
     for k,v in pairs(pfile) do
-        file_vers(k)
+        zdir[#zdir+1] = file_vers(k)
+        -- print(file_vers(k))
         -- list_files[#list_files+1]=k
     end
+    print("\n\nEt le résultat est.....................")
+    table.sort(zdir)   for i=1, #zdir do   print(zdir[i])   end
     -- zcmpt1 = 1   zrepeat()
 end
+
+--[[
+for i=1, #zdir do   print(zdir[i])   end
+]]
+
+
+
 
 function dir()
     local zdir={}
@@ -101,12 +80,13 @@ print("   file_vers('dir2.lua')")
 
 
 --[[
+dir()
+dir_vers()
+
 file_vers("dir.lua")
 file_vers("dir3.lua")
 file_vers("cat.lua")
 
-dir()
-dir_vers()
 
 
 =node.heap()
