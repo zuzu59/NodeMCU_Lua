b3e8f12d53e23d42b1753fa007efa0cbabc8c2de
Christian Zufferey <christian@zufferey.com>
2020-06-11 10:29:45 +0200

    Ajouté ma nouvelle smart bulb que j'ai achetée 25 balles chez Hornbach

--- Mesures/energy/transfo_courant_clip_1p_1/boot.lua
+++ Mesures/energy/transfo_courant_clip_1p_1/boot.lua
@@ -1,27 +1,30 @@
 -- Scripts à charger après le boot pour démarrer son projet
 
-print("\n boot.lua zf200607.1655 \n")
+print("\n boot.lua zf200610.2238 \n")
 
 -- function ztime_stamp()  return tmr.now()/1000000  end
 
-sntp.sync(nil, nil, nil, 1)
 
-function ztime_format(ztime)
-    tm = rtctime.epoch2cal(ztime + 3600)
-    return(string.format("%04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"], tm["hour"], tm["min"], tm["sec"]))
-end
 
-print(ztime_format(rtctime.get()+3600))
 
-zsec, zusec = rtctime.get()
-
-print(zsec, zusec)
-zsec=zsec+3600+zusec/1000000
-print(ztime_format(zsec))
-
-print(zsec)
-znanosec= zsec*1000000000
-print(znanosec)
+-- sntp.sync(nil, nil, nil, 1)
+-- 
+-- function ztime_format(ztime)
+--     tm = rtctime.epoch2cal(ztime + 3600)
+--     return(string.format("%04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"], tm["hour"], tm["min"], tm["sec"]))
+-- end
+-- 
+-- print(ztime_format(rtctime.get()+3600))
+-- 
+-- zsec, zusec = rtctime.get()
+-- 
+-- print(zsec, zusec)
+-- zsec=zsec+3600+zusec/1000000
+-- print(ztime_format(zsec))
+-- 
+-- print(zsec)
+-- znanosec= zsec*1000000000
+-- print(znanosec)
 
 
 
@@ -56,8 +59,9 @@ function boot()
     f="0_btn_flipflop.lua"   if file.exists(f) then dofile(f) end
     collectgarbage() print(node.heap())
     
-    f="0_get_power.lua"   if file.exists(f) then dofile(f) end
-    collectgarbage() print(node.heap())
+    zpower=0
+    -- f="0_get_power.lua"   if file.exists(f) then dofile(f) end
+    -- collectgarbage() print(node.heap())
     
     f="0_cron.lua"   if file.exists(f) then dofile(f) end
     collectgarbage() print(node.heap())
