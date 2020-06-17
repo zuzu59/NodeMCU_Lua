5842a3c2fbe443eda1808235b527f95a23eccc1b
Christian Zufferey <christian@zufferey.com>
2020-06-17 20:15:11 +0200

    Ajouté un frimware 'latest' afin de voir si j'ai toujours le même problème de reboot

--- zflash.sh
+++ zflash.sh
@@ -3,7 +3,7 @@
 
 #ATTENTION: c'est pour ma structure, il faudra donc l'adapter
 
-#zf191231.1746
+#zf200617.2010
 
 
 #test si l'argument est vide
@@ -17,6 +17,8 @@ Pour le dernier firmware à la mode:
 ./zflash.sh ../../Firmware/nodemcu-master-18-modules-2019-12-17-20-28-32-float.bin
 ./zflash.sh ../../Firmware/nodemcu-master-12-modules-2019-12-21-11-05-58-float.bin
 ./zflash.sh ../../Firmware/nodemcu-master-19-modules-2019-12-31-16-40-12-float.bin
+./zflash.sh ../../Firmware/nodemcu-master-19-modules-2020-06-17-17-22-55-float.bin
+./zflash.sh ../../Firmware/nodemcu-master-19-modules-2020-06-17-18-07-17-float.bin
 
 Pour l'ancien qui supporte encore le DS18B20:
 ./zflash.sh ../../Firmware/nodemcu-master-20-modules-2019-06-01-12-50-39-float.bin
